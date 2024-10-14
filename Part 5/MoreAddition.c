c9eb5aa03c31d7f6bdf8b5f4762cf42a4d7f556f
Lintang <saya.lintang@gmail.com>
2024-10-14 23:31:52 +0700

    Update Code

--- Part 5/MoreAddition.c
+++ Part 5/MoreAddition.c
@@ -4,9 +4,9 @@ int main(){
     int x, combi = 0;
     scanf("%d", &x);
 
-    int n = x+1;
-    while(n>0){
-        combi +=n;
+    int n = x + 1;
+    while(n > 0){
+        combi += n;
         n--;
     }
     
